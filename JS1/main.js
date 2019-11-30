11d88e1bcdbd64f41c38ca1ed119a883c83827fa
Mirjana0616 <mikamikamata@gmail.com>
2019-11-30 19:17:44 +0100

    domaci

---
+++ JS1/main.js
@@ -0,0 +1,19 @@
+
+
+var pManey = 3500;
+var jMoney = 5000;
+
+console.log(pManey > jMoney);
+
+var jAge = 38;
+var mAge = (jAge + 8) / 2 - 8;
+
+console.log("Mike is " + mAge + "years old.");
+
+var flock1 = 78;
+var flock2 = 57;
+flock1 -= 12;
+flock2 += 12;
+flock2 -= 4;
+
+console.log(flock1 >= flock2);
