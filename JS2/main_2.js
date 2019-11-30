3f0d2de554f83d4e791816fb46aae466645e81e4
Mirjana0616 <mikamikamata@gmail.com>
2019-11-30 19:21:18 +0100

    domaci

---
+++ JS2/main_2.js
@@ -0,0 +1,81 @@
+
+var n1 = 15;
+var n2 = -2;
+var n3 = 22;
+var n4 = 0;
+var n5 = 13;
+var max = n1;
+
+if(n2 > max){
+	max = n2;
+}
+
+if(n3 > max){
+	max = n3;
+}
+
+if(n4 > max){
+	max = n4;
+}
+
+if(n5 > max){
+	max = n5;
+}
+console.log("The largest of this numbers is " + max)
+
+ var a = 3;
+ var b = -7;
+ var c = 2;
+ var product = a * b * c;
+
+ if(product > 0){
+ 	console.log("This is positive number");
+ }else if(product < 0){
+ 	console.log("This is negative number");
+ }else{
+ 	console.log("This is zero");
+ }
+
+ var language = "Italian";
+
+ switch(language){
+ 	case "German":
+ 		console.log("Hello Welt");
+ 	break;
+ 	case "Italian":
+ 		console.log("Ciao mondo");
+ 	break;
+ 	case "Swedish":
+ 		console.log("Hej världen");
+ 	break;
+ 	case "French":
+ 		console.log("Bonjour tout le monde");
+ 	break;
+ 	case "English":
+ 		console.log("Hello world");
+ 	break;
+ 	default:
+ 		console.log("Zdravo svete");		
+ 			
+ }
+
+var userAge = 34;
+var age 
+userAge >= 28 ? age = "28 or older" : age = "younger than 28";
+console.log(age);
+
+if (!userAge>=28){
+	console.log ("User is less than 28 years old");
+} else if (userAge>28){
+	console.log ("User is older than 28");
+	} else{
+		console.log ("User is 28")
+	}
+
+
+
+
+
+
+
+ 
